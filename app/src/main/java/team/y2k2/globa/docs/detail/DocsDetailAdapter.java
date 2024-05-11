8f558a90f712b4a97efea023f75f6c86bdf5682c
haebun <mediafactory1279@gmail.com>
2024-05-11 14:06:36 +0900

    feat: 댓글 레이아웃 구현 1차
    현재 하이라이트 문장 클릭 시 댓글 다이얼로그를 불러옴

--- app/src/main/java/team/y2k2/globa/docs/detail/DocsDetailAdapter.java
+++ app/src/main/java/team/y2k2/globa/docs/detail/DocsDetailAdapter.java
@@ -2,15 +2,21 @@ package team.y2k2.globa.docs.detail;
 
 import android.graphics.Color;
 import android.text.SpannableString;
+import android.text.TextPaint;
 import android.text.style.BackgroundColorSpan;
+import android.text.style.ClickableSpan;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.ArrayList;
 
@@ -27,30 +33,59 @@ public class DocsDetailAdapter extends RecyclerView.Adapter<DocsDetailAdapter.Ad
 
     @NonNull
     @Override
-    public DocsDetailAdapter.AdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public AdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_docs_detail, parent, false);
-        return new DocsDetailAdapter.AdapterViewHolder(view);
+        return new AdapterViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DocsDetailAdapter.AdapterViewHolder holder, int position) {
-
-        if(position == 0) {
+    public void onBindViewHolder(@NonNull AdapterViewHolder holder, int position) {
+        if (position == 0) {
             DocsDetailHighlightModel model = new DocsDetailHighlightModel();
             String description = items.get(position).getDescription();
             SpannableString highlightString = new SpannableString(description);
 
-            for(int i = 0; i < model.getItems().size(); i++) {
+            for (int i = 0; i < model.getItems().size(); i++) {
                 DocsDetailHighlightItem highlight = model.getItems().get(i);
 
-                highlightString.setSpan(new BackgroundColorSpan(holder.itemView.getResources().getColor(highlight.getHighlightColor())), highlight.getStartIndex(), highlight.getEndIndex(), 0);
-                highlightString.setSpan(new ForegroundColorSpan(Color.WHITE), highlight.getStartIndex(), highlight.getEndIndex(), 0);
+                int startIdx = highlight.getStartIndex();
+                int endIdx = highlight.getEndIndex();
+
+                // 클릭 가능한 Span 설정
+                ClickableSpan clickableSpan = new ClickableSpan() {
+                    @Override
+                    public void onClick(View widget) {
+                        // 클릭 시 동작할 내용 구현
+                        Log.d(getClass().getName(), "클릭됨");
+                        Toast.makeText(widget.getContext(), "SpannableString 클릭됨", Toast.LENGTH_SHORT).show();
+
+                        BottomSheetDialog commentBottomSheet = new BottomSheetDialog(holder.itemView.getContext());
+                        commentBottomSheet.setContentView(R.layout.dialog_comment);
+                        commentBottomSheet.show();
+
+                    }
+
+                    @Override
+                    public void updateDrawState(@NonNull TextPaint ds) {
+                        super.updateDrawState(ds);
+                        ds.setColor(Color.WHITE);
+                        ds.setUnderlineText(false);
+                    }
+                };
+
+                BackgroundColorSpan backgroundColorSpan = new BackgroundColorSpan(holder.itemView.getResources().getColor(highlight.getHighlightColor()));
+
+                highlightString.setSpan(backgroundColorSpan, startIdx, endIdx, 0);
+                highlightString.setSpan(clickableSpan, startIdx, endIdx, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+
+
             }
             holder.description.setText(highlightString);
-        }
-        else
+            holder.description.setMovementMethod(android.text.method.LinkMovementMethod.getInstance()); // SpannableString을 클릭 가능하게 만듦
+        } else {
             holder.description.setText(items.get(position).getDescription());
-
+        }
 
         holder.title.setText(items.get(position).getTitle());
         holder.time.setText(items.get(position).getTime());
@@ -60,6 +95,7 @@ public class DocsDetailAdapter extends RecyclerView.Adapter<DocsDetailAdapter.Ad
     public int getItemCount() {
         return (null != items ? items.size() : 0);
     }
+
     public static class AdapterViewHolder extends RecyclerView.ViewHolder {
         TextView title;
         TextView time;
