167f999347dbc0b23516c54b3c00575f53bcf3ab
HaeBun <mediafactory1279@gmail.com>
2024-09-05 02:17:07 +0900

    fix : 검색 SQLite 미동작 문제
    검색창에 타이핑 시 SQLite를 우선 조회하지  않는 현상을 수정

--- app/src/main/java/team/y2k2/globa/main/search/SearchActivity.java
+++ app/src/main/java/team/y2k2/globa/main/search/SearchActivity.java
@@ -24,7 +24,9 @@ import team.y2k2.globa.sql.RecordDB;
 
 public class SearchActivity extends AppCompatActivity {
 
+    RecordDB recordDB;
     ActivitySearchBinding binding;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,63 +36,10 @@ public class SearchActivity extends AppCompatActivity {
             finish();
         });
 
-        RecordDB recordDB = new RecordDB(this);
-
-        binding.edittextSearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                List<Record> records = recordDB.onSearch(String.valueOf(s));
-                ArrayList<SearchDocsItem> docsItems = new ArrayList<>();
-
-                if(docsItems.size() != 0)
-                    for(int i = 0; i < records.size(); i++) {
-                        Record record = records.get(i);
-                        String folderId = record.getFolderId();
-                        String recordId = record.getRecordId();
-                        String title = record.getTitle();
-                        String datetime = record.getCreatedTime();
-
-                        SearchDocsItem item = new SearchDocsItem(folderId, recordId, title, datetime);
-                        docsItems.add(item);
-                    }
-
-                Log.d(getClass().getName(), docsItems.size() + "개 확인됨");
-
-                SearchDocsAdapter adapter = new SearchDocsAdapter(docsItems);
-
-                LinearLayoutManager layoutManager = new LinearLayoutManager(binding.getRoot().getContext());
-                layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-                binding.recyclerviewSearchHistory.setAdapter(adapter);
-                binding.recyclerviewSearchHistory.setLayoutManager(layoutManager);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-//                if (s.length() > 32) {
-//                    binding.edittextFolderNameInputname.removeTextChangedListener(this);
-//                    String text = s.toString().substring(0, 32);
-//                    binding.edittextFolderNameInputname.setText(text);
-//                    binding.edittextFolderNameInputname.setSelection(text.length());
-//                    binding.edittextFolderNameInputname.addTextChangedListener(this);
-//                }
-//
-//                if (s.length() <= 32) {
-//                    binding.textviewFolderNameCount.setText(s.length() + "/32");
-//                    binding.textviewFolderNameChangeConfirm.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.primary));
-//                }
-//
-//                if (s.length() == 0) {
-//                    binding.textviewFolderNameChangeConfirm.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.gray));
-//                }
-
-            }
-        });
+        recordDB = new RecordDB(this);
 
         binding.edittextSearch.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+
         // 사용자가 확인 동작을 실행 시
         binding.edittextSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -98,6 +47,30 @@ public class SearchActivity extends AppCompatActivity {
 
                 if(actionId == EditorInfo.IME_ACTION_DONE) {
                     ArrayList<SearchDocsItem> docsItems = getRecordSearchResultToAPI(v.getText().toString());
+                    SearchDocsAdapter adapter = new SearchDocsAdapter(docsItems);
+
+                    LinearLayoutManager layoutManager = new LinearLayoutManager(binding.getRoot().getContext());
+                    layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+                    binding.recyclerviewSearchHistory.setAdapter(adapter);
+                    binding.recyclerviewSearchHistory.setLayoutManager(layoutManager);
+                }
+                else {
+                    CharSequence s = binding.edittextSearch.getText();
+                    List<Record> records = recordDB.onSearch(String.valueOf(s));
+                    ArrayList<SearchDocsItem> docsItems = new ArrayList<>();
+
+                    if(docsItems.size() != 0)
+                        for(int i = 0; i < records.size(); i++) {
+                            Record record = records.get(i);
+                            String folderId = record.getFolderId();
+                            String recordId = record.getRecordId();
+                            String title = record.getTitle();
+                            String datetime = record.getCreatedTime();
+
+                            SearchDocsItem item = new SearchDocsItem(folderId, recordId, title, datetime);
+                            docsItems.add(item);
+                        }
 
                     Log.d(getClass().getName(), docsItems.size() + "개 확인됨");
 
@@ -108,15 +81,53 @@ public class SearchActivity extends AppCompatActivity {
 
                     binding.recyclerviewSearchHistory.setAdapter(adapter);
                     binding.recyclerviewSearchHistory.setLayoutManager(layoutManager);
+
                 }
 
                 return false;
             }
         });
 
+        binding.edittextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                updateSearchResults(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+
         setContentView(binding.getRoot());
     }
 
+    private void updateSearchResults(CharSequence s) {
+        List<Record> records = recordDB.onSearch(String.valueOf(s));
+        ArrayList<SearchDocsItem> docsItems = new ArrayList<>();
+
+        for (int i = 0; i < records.size(); i++) {
+            Record record = records.get(i);
+            String folderId = record.getFolderId();
+            String recordId = record.getRecordId();
+            String title = record.getTitle();
+            String datetime = record.getCreatedTime();
+
+            SearchDocsItem item = new SearchDocsItem(folderId, recordId, title, datetime);
+            docsItems.add(item);
+        }
+
+        Log.d(getClass().getName(), docsItems.size() + "개 확인됨");
+
+        SearchDocsAdapter adapter = new SearchDocsAdapter(docsItems);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(binding.getRoot().getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        binding.recyclerviewSearchHistory.setAdapter(adapter);
+        binding.recyclerviewSearchHistory.setLayoutManager(layoutManager);
+    }
 
     public ArrayList<SearchDocsItem> getRecordSearchResultToAPI(String keyword) {
         ArrayList<SearchDocsItem> docsItems = new ArrayList<>();
@@ -150,4 +161,6 @@ public class SearchActivity extends AppCompatActivity {
         }
         return docsItems;
     }
+
+
 }
