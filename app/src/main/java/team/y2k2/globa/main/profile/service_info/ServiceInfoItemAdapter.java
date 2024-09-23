fd3d6e7c969cb5360e39833b70c2f2c82bc6a894
HaeBun <mediafactory1279@gmail.com>
2024-09-24 02:38:50 +0900

    feat: 개인정보처리방침 및 서드 파티 공지 추가
    이제 서비스 정보에서 개인정보처리방침 및 서드 파티(오픈소스 라이선스)에 대한 정보가 웹뷰로 제공됩니다.

--- app/src/main/java/team/y2k2/globa/main/profile/service_info/ServiceInfoItemAdapter.java
+++ app/src/main/java/team/y2k2/globa/main/profile/service_info/ServiceInfoItemAdapter.java
@@ -1,9 +1,13 @@
 package team.y2k2.globa.main.profile.service_info;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,8 +20,10 @@ import team.y2k2.globa.R;
 
 public class ServiceInfoItemAdapter extends RecyclerView.Adapter<ServiceInfoItemAdapter.AdapterViewHolder> {
     ArrayList<ServiceInfoItem> items;
+    Activity activity;
 
-    public ServiceInfoItemAdapter(ArrayList<ServiceInfoItem> items) {
+    public ServiceInfoItemAdapter(Activity activity, ArrayList<ServiceInfoItem> items) {
+        this.activity = activity;
         this.items = items;
     }
 
@@ -33,13 +39,15 @@ public class ServiceInfoItemAdapter extends RecyclerView.Adapter<ServiceInfoItem
         holder.title.setText(items.get(position).getTitle());
         holder.description.setText(items.get(position).getDescription());
 
-        if(items.get(position).getActivity() == null)
-            return;
+        String url = items.get(position).getUrl();
 
-//        holder.layout.setOnClickListener(view -> {
-//            Intent intent = new Intent(holder.itemView.getContext(), items.get(position).getActivity().getClass());
-//            holder.itemView.getContext().startActivity(intent);
-//        });
+        if(url != null)
+            holder.layout.setOnClickListener(view -> {
+                Intent intent = new Intent(activity, WebViewActivity.class);
+                intent.putExtra("url", url);
+                activity.startActivity(intent);
+            });
+
     }
 
     @Override
@@ -51,14 +59,14 @@ public class ServiceInfoItemAdapter extends RecyclerView.Adapter<ServiceInfoItem
         TextView title;
         TextView description;
 
-        ConstraintLayout layout;
+        LinearLayout layout;
 
         public AdapterViewHolder(@NonNull View itemView) {
             super(itemView);
 
             title = itemView.findViewById(R.id.textview_item_service_info_title);
             description = itemView.findViewById(R.id.textview_item_service_info_description);
-//            layout = itemView.findViewById(R.id.item_profile_items);
+            layout = itemView.findViewById(R.id.linearlayout_item_service_info_title);
         }
     }
 }
--- app/src/main/java/team/y2k2/globa/main/profile/service_info/ServiceInfoModel.java
+++ app/src/main/java/team/y2k2/globa/main/profile/service_info/ServiceInfoModel.java
@@ -11,10 +11,10 @@ public class ServiceInfoModel {
     public ServiceInfoModel() {
         items = new ArrayList<>();
 
-        items.add(new ServiceInfoItem(R.string.profile_service_info_1_title, R.string.profile_service_info_1_description));
-        items.add(new ServiceInfoItem(R.string.profile_service_info_2_title, R.string.profile_service_info_2_description));
-        items.add(new ServiceInfoItem(R.string.profile_service_info_3_title, R.string.profile_service_info_3_description));
-        items.add(new ServiceInfoItem(R.string.profile_service_info_4_title, R.string.profile_service_info_4_description));
+        items.add(new ServiceInfoItem(R.string.profile_service_info_1_title, R.string.profile_service_info_1_description, null));
+        items.add(new ServiceInfoItem(R.string.profile_service_info_2_title, R.string.profile_service_info_2_description, "https://joyous-coelurus-23b.notion.site/5e8ff23d34d14c128e21e30c71a2b0bc?pvs=4"));
+        items.add(new ServiceInfoItem(R.string.profile_service_info_3_title, R.string.profile_service_info_3_description, null));
+        items.add(new ServiceInfoItem(R.string.profile_service_info_4_title, R.string.profile_service_info_4_description, "https://joyous-coelurus-23b.notion.site/eb062dceb56e4750be51b25690e1bd91?pvs=4"));
     }
 
     public ArrayList<ServiceInfoItem> getItems() {
