d0724deb77be2a2cb51e6ff1c10609211748ca1d
seungyong <seungyong20@naver.com>
2024-09-24 03:48:59 +0900

    차트 글자 크기 10f로 변경

--- app/src/main/java/team/y2k2/globa/main/statistics/StatisticsFragment.java
+++ app/src/main/java/team/y2k2/globa/main/statistics/StatisticsFragment.java
@@ -240,7 +240,7 @@ public class StatisticsFragment extends Fragment {
         // 각 막대의 색상 설정
         int[] barColors = new int[]{Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#E8C1A0"), Color.parseColor("#B4622F")};
         dataSet.setColors(barColors);
-        dataSet.setValueTextSize(12f);
+        dataSet.setValueTextSize(10f);
 
         // 데이터 객체 생성
         BarData data = new BarData(dataSet);
