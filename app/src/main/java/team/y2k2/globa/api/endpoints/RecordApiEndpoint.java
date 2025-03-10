2309f8b5515df4d9ab17219307129f2a4201ecc1
haebun <mediafactory1279@gmail.com>
2025-03-10 15:06:41 +0900

    fix : Folder, API Path 내 파라미터명 수정
    엔드포인트 경로 내 파라미터명 수정

--- app/src/main/java/team/y2k2/globa/api/endpoints/RecordApiEndpoint.java
+++ app/src/main/java/team/y2k2/globa/api/endpoints/RecordApiEndpoint.java
@@ -2,13 +2,13 @@ package team.y2k2.globa.api.endpoints;
 
 public class RecordApiEndpoint {
 //    문서 링크 공유 취소
-    public static final String DELETE_RECORD_SHARE_LINK = "/folder/{folderId}/record/{recordId}/link";
+    public static final String DELETE_RECORD_SHARE_LINK = "/folder/{folder_id}/record/{record_id}/link";
 //    문서 삭제
-    public static final String DELETE_RECORD = "/folder/{folderId}/record/{recordId}";
+    public static final String DELETE_RECORD = "/folder/{folder_id}/record/{record_id}";
 //    폴더 내 녹음 파일 조회
-    public static final String GET_RECORD_IN_FOLDER = "/folder/{folderId}/record";
+    public static final String GET_RECORD_IN_FOLDER = "/folder/{folder_id}/record";
 //    퀴즈 조회
-    public static final String GET_RECORD_QUIZ = "/folder/{folderId}/record/{recordId}/quiz";
+    public static final String GET_RECORD_QUIZ = "/folder/{folder_id}/record/{record_id}/quiz";
 //    모든 녹음 파일 조회
     public static final String GET_RECORD_ALL = "/record";
 //    공유 하는 문서 조회
@@ -18,19 +18,19 @@ public class RecordApiEndpoint {
 //    공유 받는 문서 조회
     public static final String GET_RECORD_RECEIVING = "/record/receiving";
 //    녹음 파일 상세 조회
-    public static final String GET_RECORD_DETAIL_IN_FOLDER = "/folder/{folderId}/record/{recordId}";
+    public static final String GET_RECORD_DETAIL_IN_FOLDER = "/folder/{folder_id}/record/{record_id}";
 //    문서 내 시각화 자료 조회
-    public static final String GET_RECORD_ANALYSIS_IN_FOLDER = "/folder/{folderId}/record/{recordId}/analysis";
+    public static final String GET_RECORD_ANALYSIS_IN_FOLDER = "/folder/{folder_id}/record/{record_id}/analysis";
 //    공부 시간 수정
-    public static final String PATCH_RECORD_STUDY = "/folder/{folderId}/record/{recordId}/study";
+    public static final String PATCH_RECORD_STUDY = "/folder/{folder_id}/record/{record_id}/study";
 //    문서 이름 수정
-    public static final String PATCH_RECORD_NAME = "/folder/{folderId}/record/{recordId}/name";
+    public static final String PATCH_RECORD_NAME = "/folder/{folder_id}/record/{record_id}/name";
 //    문서 폴더 이동
-    public static final String PATCH_RECORD_MOVE_FOLDER = "/folder/{folderId}/record/{recordId}/folder";
+    public static final String PATCH_RECORD_MOVE_FOLDER = "/folder/{folder_id}/record/{record_id}/folder";
 //    문서 추가
-    public static final String POST_RECORD = "/folder/{folderId}/record";
+    public static final String POST_RECORD = "/folder/{folder_id}/record";
 //    퀴즈 결과 추가
-    public static final String POST_RECORD_QUIZ = "/folder/{folderId}/record/{recordId}/quiz";
+    public static final String POST_RECORD_QUIZ = "/folder/{folder_id}/record/{record_id}/quiz";
 //    문서 링크 공유
-    public static final String POST_FOLDER_RECORD_LINK = "/folder/{folderId}/record/{recordId}/link";
+    public static final String POST_FOLDER_RECORD_LINK = "/folder/{folder_id}/record/{record_id}/link";
 }
